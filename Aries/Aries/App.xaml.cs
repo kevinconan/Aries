25fe842a63f39e18dc56312e270f874999412a43
Kevinconan <kevinlyhan@gmail.com>
2016-04-23 23:19:58 +0800

    AllinOne

--- Aries/Aries/App.xaml.cs
+++ Aries/Aries/App.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,41 @@ namespace Aries
     /// </summary>
     public partial class App : Application
     {
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            var executingAssemblyName = executingAssembly.GetName();
+            var resName = executingAssemblyName.Name + ".resources";
+
+            AssemblyName assemblyName = new AssemblyName(args.Name); string path = "";
+            if (resName == assemblyName.Name)
+            {
+                path = executingAssemblyName.Name + ".g.resources"; ;
+            }
+            else
+            {
+                path = assemblyName.Name + ".dll";
+                if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false)
+                {
+                    path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+                }
+            }
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream == null)
+                    return null;
+
+                byte[] assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+        }
     }
 }
