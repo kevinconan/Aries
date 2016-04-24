2d1eb6a1521429c62b6dd88108cfd1e35b038a22
Kevinconan <kevinlyhan@gmail.com>
2016-04-24 15:51:16 +0800

    新增Edit窗口

---
+++ Aries/Aries.UI/EditServerConfigWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace Aries
+{
+    /// <summary>
+    /// Interaction logic for EditServerConfigWindow.xaml
+    /// </summary>
+    public partial class EditServerConfigWindow : MetroWindow
+    {
+        public EditServerConfigWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
